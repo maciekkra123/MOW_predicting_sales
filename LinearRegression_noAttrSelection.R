47551e09884ed2154e1a71ee5a1d0f655188832f
maciekkra <maciek_krasowski@yahoo.pl>
2018-04-28 12:54:53 +0200

    Path tests

--- LinearRegression_noAttrSelection.R
+++ LinearRegression_noAttrSelection.R
@@ -1,3 +1,6 @@
 library(caret)
-Data_all <- read.csv("~/Documents/MOW/MOW_projekt/Data_raw/Dataset_old.csv", na.strings="NaN")
-#View(Data_all) #wglad w dane (widok tabelaryczny)
+# Data_all <- read.csv("../Data_raw/Dataset_old.csv", na.strings="NaN")
+# View(Data_all) #wglad w dane (widok tabelaryczny)
+
+test_kaggle <- read.csv("Kaggle_data/TestDataset.csv")
+View(test_kaggle)
