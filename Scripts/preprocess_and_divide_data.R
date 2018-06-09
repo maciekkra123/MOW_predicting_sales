17419006b813fe1067d95b6cc7685bb13c4836f0
olchas <kolszewski.94@wp.pl>
2018-06-09 22:48:37 +0200

    Finalna wersja skryptow

--- Scripts/preprocess_and_divide_data.R
+++ Scripts/preprocess_and_divide_data.R
@@ -1,4 +1,5 @@
-#ten skrypt preporcesuje dane wejsciowe, zastepujac NaNy, i rodziela je na zbiory treningowy i testowy
+#ten skrypt preprocesuje dane wejsciowe, zastepujac NaNy zerem i srednia, i rodziela je na zbiory treningowy i testowy
+library(dummies)
 
 input_data = read.csv("../Data_raw/Dataset_old.csv", na.strings="NaN")
 
@@ -7,10 +8,6 @@ date_variables_names = c("Date_1", "Date_2")
 outcome_variables_names = paste("Outcome_M", 1:12, sep="")
 
 #outcome_variables_names = grep("Outcome_", names(input_data))
-
-cat_variables_columns = grep("Cat_", names(input_data))
-
-quant_variables_columns = grep("Quan", names(input_data))
 
 #usuniecie dwoch wierszy z brakujaca informacja o zmiennej Date_2
 input_data = input_data[complete.cases(input_data[date_variables_names]),]
@@ -33,48 +30,92 @@ input_data = cbind(input_data, Day_Of_Year_1, Day_Of_Year_2, Day_Of_Week_1, Day_
 #usuniecie kolumn z zerowa wariancja (o tych samych wartosciach), testy wykazaly, ze Boruta je zawsze odrzuca (i slusznie)
 input_data = input_data[,apply(input_data, 2, var, na.rm=TRUE) != 0]
 
+cat_variables_columns = grep("Cat_", names(input_data))
+
+cat_variables_names = names(input_data)[cat_variables_columns]
+
+quant_variables_columns = grep("Quan", names(input_data))
+
 mean_nan_input_data = input_data
+zero_nan_input_data = input_data
 
 #zastapienie NaNow w zmiennych Quan ich srednimi <- inna opcja jest zastapienie NaNow zerami, potem zostawimy lepsza z nich
 #zastapienie Nanow jest niezbedne dla dzialania Boruty
 #do obliczenia sredniej uzywana jest cala informacja (rowniez z wierszy, ktore znajda sie w testsecie), dlatego wykonywane jest teraz
 for(i in quant_variables_columns){
   mean_nan_input_data[is.na(mean_nan_input_data[,i]),i] = mean(mean_nan_input_data[,i], na.rm = TRUE)
-  #input_data[is.na(input_data[,i]),i] = 0
+  zero_nan_input_data[is.na(zero_nan_input_data[,i]),i] = 0
 }
 
-#zastapienie NaNow w zmiennych Cat ich medianami -> nie ma takich przypadkow
-#for(i in cat_variables_columns){
-#    input_data[is.na(input_data[,i]),i] = median(as.numeric(as.character(input_data[,i])), na.rm = TRUE)
-#}
-
 #znalezienie wierszy z pelna informacja o sprzedazy
-complete_cases = complete.cases(input_data[outcome_variables_names])
+complete_cases = complete.cases(zero_nan_input_data[outcome_variables_names])
 
-complete_outcome_information = input_data[complete_cases,]
+zero_nan_complete_outcome_information = zero_nan_input_data[complete_cases,]
 mean_nan_complete_outcome_information = mean_nan_input_data[complete_cases,]
 
 #znalezienie wierszy z niepelna informacja o sprzedazy - jedna ze zmiennych Outcome == NaN
-incomplete_outcome_information = input_data[!complete_cases,]
+zero_nan_incomplete_outcome_information = zero_nan_input_data[!complete_cases,]
 mean_nan_incomplete_outcome_information = mean_nan_input_data[!complete_cases,]
 
 #losowa probka 10% wierszy o pelnej informacji sprzedazy do testow, uzywamy tej samej probki
-random_sample = sample(1:nrow(complete_outcome_information), round(0.1*nrow(complete_outcome_information)))
+random_sample = sample(1:nrow(zero_nan_complete_outcome_information), round(0.1*nrow(zero_nan_complete_outcome_information)))
 
 #polaczenie tej probki z wierszami o niepelnej informacji sprzedazy tworzy zbior testowy
-test_data = rbind(complete_outcome_information[random_sample,],incomplete_outcome_information)
+zero_nan_test_data = rbind(zero_nan_complete_outcome_information[random_sample,],zero_nan_incomplete_outcome_information)
 mean_nan_test_data = rbind(mean_nan_complete_outcome_information[random_sample,],mean_nan_incomplete_outcome_information)
 
 #do trainsetu trafiaja wszystkie wiersze z pelna informacja o sprzedazy oprocz tej probki
-training_data = complete_outcome_information[-random_sample,]
+zero_nan_training_data = zero_nan_complete_outcome_information[-random_sample,]
 mean_nan_training_data = mean_nan_complete_outcome_information[-random_sample,]
 
-write.csv(training_data, file = "../Data_raw/RawTrainingData.csv", row.names=FALSE)
-write.csv(test_data, file = "../Data_raw/RawTestData.csv", row.names=FALSE)
+write.csv(zero_nan_training_data, file = "../Data_raw/ZeroNanTrainingData.csv", row.names=FALSE)
+write.csv(zero_nan_test_data, file = "../Data_raw/ZeroNanTestData.csv", row.names=FALSE)
 
 write.csv(mean_nan_training_data, file = "../Data_raw/MeanNanTrainingData.csv", row.names=FALSE)
 write.csv(mean_nan_test_data, file = "../Data_raw/MeanNanTestData.csv", row.names=FALSE)
 
-#znajdz liczbe nanow w kolumnie (dla celow pogladowych)
-#number_of_nans = apply(apply(input_data,2,is.na),2,sum)
-#number_of_nans = number_of_nans[which(number_of_nans>0)]
+training_datasets = list(zero_nan_training_data, mean_nan_training_data)
+test_datasets = list(zero_nan_test_data, mean_nan_test_data)
+directories_names = list("no_boruta_zero_nan", "no_boruta_mean_nan")
+
+for(i in 1:2) {
+
+  training_dataset = training_datasets[[i]]
+  test_dataset = test_datasets[[i]]
+  directory_name = directories_names[[i]]
+  
+  for (k in 1:12){
+    selected_training_data = cbind(training_dataset[k], training_dataset[13:length(training_dataset)])
+    selected_test_data = cbind(test_dataset[k], test_dataset[13:length(test_dataset)])
+    
+    write.csv(selected_training_data, file = sprintf("../Data_raw/%s/TrainingData_Outcome_M%s.csv", directory_name, k), row.names=FALSE)
+    write.csv(selected_test_data, file = sprintf("../Data_raw/%s/TestData_Outcome_M%s.csv", directory_name, k), row.names=FALSE)
+    
+    selected_training_dummy_variables= c()
+    selected_test_dummy_variables= c()
+    
+    for (cat_variable in cat_variables_names){
+      selected_training_dummy_variables = cbind(selected_training_dummy_variables, dummy(cat_variable, training_dataset, sep="_"))
+      selected_test_dummy_variables = cbind(selected_test_dummy_variables, dummy(cat_variable, test_dataset, sep="_"))
+    }
+    
+    unique_to_training_data = setdiff(colnames(selected_training_dummy_variables), colnames(selected_test_dummy_variables))
+    unique_to_test_data = setdiff(colnames(selected_test_dummy_variables), colnames(selected_training_dummy_variables))
+    
+    selected_test_dummy_variables = data.frame(selected_test_dummy_variables)
+    selected_training_dummy_variables = data.frame(selected_training_dummy_variables)
+    
+    #dodanie brakujacych dummy variables do danych testowych
+    selected_test_dummy_variables[, unique_to_training_data] = 0
+    
+    #usuniecie dummy variables nieobecnych w danych treningowych
+    selected_test_dummy_variables = selected_test_dummy_variables[, !(names(selected_test_dummy_variables) %in% unique_to_test_data)]
+    
+    selected_dummy_training_data = cbind(selected_training_data[, !(names(selected_training_data) %in% cat_variables_names)], selected_training_dummy_variables)
+    selected_dummy_test_data = cbind(selected_test_data[, !(names(selected_test_data) %in% cat_variables_names)], selected_test_dummy_variables)
+    
+    write.csv(selected_dummy_training_data, file = sprintf("../Data_raw/%s/DummyTrainingData_Outcome_M%s.csv", directory_name, k), row.names=FALSE)
+    write.csv(selected_dummy_test_data, file = sprintf("../Data_raw/%s/DummyTestData_Outcome_M%s.csv", directory_name , k), row.names=FALSE)
+    
+    }
+}
--- Scripts/run_boruta.R
+++ Scripts/run_boruta.R
@@ -1,203 +1,142 @@
-#ten skrypt przeprowadza selekcje atrybutow wykorzystujac algorytm Boruta
+#ten skrypt przeprowadza selekcje atrybutow wykorzystujac algorytm Boruta dla czterech wariantów danych
 
 library(Boruta)
 library(dummies)
 
-traindata_mean = read.csv("../Data_raw/MeanNanTrainingData.csv", header = TRUE)
-traindata_zero = read.csv("../Data_raw/RawTrainingData.csv", header = TRUE)
+mean_nan_training_data = read.csv("../Data_raw/MeanNanTrainingData.csv", header = TRUE)
+zero_nan_training_data = read.csv("../Data_raw/ZeroNanTrainingData.csv", header = TRUE)
 
-test_data = read.csv("../Data_raw/RawTestData.csv")
-
-#zamiana zmiennych Cat z typu integer na typ factor
-cat_variables_columns = grep("Cat_", names(traindata_zero))
-cat_variables_names = names(traindata_zero)[cat_variables_columns]
-
-quant_variables_columns = grep("Quan", names(traindata_zero))
-
-#zastapienie NaNow zerami
-for(i in quant_variables_columns){
-  traindata_zero[is.na(traindata_zero[,i]),i] = 0
-  test_data[is.na(test_data[,i]),i] = 0
-}
+mean_nan_test_data = read.csv("../Data_raw/MeanNanTestData.csv", header = TRUE)
+zero_nan_test_data = read.csv("../Data_raw/ZeroNanTestData.csv", header = TRUE)
 
 #zamiana zmiennych Cat z typu integer na typ factor
-traindata_mean[,cat_variables_columns] = lapply(traindata_mean[,cat_variables_columns], factor)
-traindata_zero[,cat_variables_columns] = lapply(traindata_zero[,cat_variables_columns], factor)
+cat_variables_columns = grep("Cat_", names(zero_nan_training_data))
+cat_variables_names = names(zero_nan_training_data)[cat_variables_columns]
 
-#podobno dla wielu algorytmow problemem sa sytuacje, w ktorych w danych testowych pojawia sie wartosc atrybutu kategorycznego nieobecna w danych treningowych
-#rozwiazanie - zamiana zmiennych kategorycznych na dummy variables
-dummy_variables= c()
+quant_variables_columns = grep("Quan", names(zero_nan_training_data))
 
-for (cat_variable in cat_variables_names){
-  dummy_variables = cbind(dummy_variables, dummy(cat_variable, traindata_zero, sep="_"))
-}
-
-dummy_traindata_zero = cbind(traindata_zero[ , !(names(traindata_zero) %in% cat_variables_names)],dummy_variables)
-#1763 atrybutow! po usunieciu duplikatow nadal jest ich 1292 (ewentualnie robic to po borucie?)
+#zamiana zmiennych Cat z typu integer na typ factor
+mean_nan_training_data[,cat_variables_columns] = lapply(mean_nan_training_data[,cat_variables_columns], factor)
+zero_nan_training_data[,cat_variables_columns] = lapply(zero_nan_training_data[,cat_variables_columns], factor)
 
 #usuniecie kolumn bedacych duplikatami
-removed_traindata_mean = traindata_mean[, !duplicated(t(traindata_mean))]
-removed_traindata_zero = traindata_zero[, !duplicated(t(traindata_zero))]
+removed_mean_nan_training_data = mean_nan_training_data[, !duplicated(t(mean_nan_training_data))]
+removed_zero_nan_training_data = zero_nan_training_data[, !duplicated(t(zero_nan_training_data))]
 
-boruta_results = matrix(rep(list(), 120),nrow = 10, ncol = 12)
-attributes_zero = traindata_zero[,14:length(traindata_zero)]
+boruta_results_zero_nan = matrix(rep(list(), 120),nrow = 10, ncol = 12)
+attributes_zero = zero_nan_training_data[,13:length(zero_nan_training_data)]
 
 #pierwsza kolumna to indeks
-#dla kazdego z 12 atrybutow Outcome Boruta jest puszczana 10 razy, jeden run to okolo 3 minuty obliczen
-for(i in 2:13) {
-  single_outcome = traindata_zero[,i]
+#dla kazdego z 12 atrybutow Outcome Boruta jest uruchamiana 10 razy, jeden run to okolo 3 minut obliczen
+for(i in 1:12) {
+  single_outcome = zero_nan_training_data[,i]
   for(j in 1:10) {
-    boruta_results[[(i-2)*10+j]] = getSelectedAttributes(TentativeRoughFix(Boruta(attributes_zero, single_outcome, maxRuns = 100)))
+    boruta_results_zero_nan[[(i-1)*10+j]] = getSelectedAttributes(TentativeRoughFix(Boruta(attributes_zero, single_outcome, maxRuns = 100)))
   }
 }
 
 boruta_results_mean_nan = matrix(rep(list(), 120),nrow = 10, ncol = 12)
-attributes_mean = traindata_mean[,14:length(traindata_mean)]
+attributes_mean = mean_nan_training_data[,13:length(mean_nan_training_data)]
 
-for(i in 2:13) {
-  single_outcome = traindata_mean[,i]
+for(i in 1:12) {
+  single_outcome = mean_nan_training_data[,i]
   for(j in 1:10) {
-    boruta_results_mean_nan[[(i-2)*10+j]] = getSelectedAttributes(TentativeRoughFix(Boruta(attributes_mean, single_outcome, maxRuns = 100)))
-  }
-}
-
-boruta_results_removed_mean_nan = matrix(rep(list(), 120),nrow = 10, ncol = 12)
-removed_attributes_mean = removed_mean[,14:length(removed_mean)]
-
-
-for(i in 2:13) {
-  single_outcome = removed_mean[,i]
-  for(j in 1:10) {
-    boruta_results_removed_mean_nan[[(i-2)*10+j]] = getSelectedAttributes(TentativeRoughFix(Boruta(removed_attributes_mean, single_outcome, maxRuns = 100)))
+    boruta_results_mean_nan[[(i-1)*10+j]] = getSelectedAttributes(TentativeRoughFix(Boruta(attributes_mean, single_outcome, maxRuns = 100)))
   }
 }
 
 boruta_results_removed_zero_nan = matrix(rep(list(), 120),nrow = 10, ncol = 12)
-removed_attributes_zero = removed_zero[,14:length(removed_zero)]
+removed_attributes_zero = removed_zero_nan_training_data[,13:length(removed_zero_nan_training_data)]
 
-for(i in 2:13) {
-  single_outcome = removed_zero[,i]
+for(i in 1:12) {
+  single_outcome = removed_zero_nan_training_data[,i]
   for(j in 1:10) {
-    boruta_results_removed_zero_nan[[(i-2)*10+j]] = getSelectedAttributes(TentativeRoughFix(Boruta(removed_attributes_zero, single_outcome, maxRuns = 100)))
+    boruta_results_removed_zero_nan[[(i-1)*10+j]] = getSelectedAttributes(TentativeRoughFix(Boruta(removed_attributes_zero, single_outcome, maxRuns = 100)))
   }
 }
 
-#wybranie atrybutow wspolnych dla wszystkich prob dla danej zmiennej outcome
-column_selected_attributes = matrix(rep(list(), 12),nrow = 10, ncol = 1)
-for (k in 1:12){
-  intersect_selected_attributes = boruta_results[[(k-1)*10 + 1]]
-  for (i in 2:10) {
-    intersect_selected_attributes = intersect(boruta_results[[(k-1)*10 + i]],intersect_selected_attributes)
+boruta_results_removed_mean_nan = matrix(rep(list(), 120),nrow = 10, ncol = 12)
+removed_attributes_mean = removed_mean_nan_training_data[,13:length(removed_mean_nan_training_data)]
+
+for(i in 1:12) {
+  single_outcome = removed_mean_nan_training_data[,i]
+  for(j in 1:10) {
+    boruta_results_removed_mean_nan[[(i-1)*10+j]] = getSelectedAttributes(TentativeRoughFix(Boruta(removed_attributes_mean, single_outcome, maxRuns = 100)))
   }
-  column_selected_attributes[[k]] = intersect_selected_attributes
 }
 
-#polaczenie wszystkich atrybutow dla wszystkich kolumn
-union_selected_attributes = column_selected_attributes[[1]]
-for (k in 2:12){
-  union_selected_attributes = union(column_selected_attributes[[k]],union_selected_attributes)
-}
+boruta_results = list(boruta_results_zero_nan, boruta_results_mean_nan, boruta_results_removed_zero_nan, boruta_results_removed_mean_nan)
+training_datasets = list(zero_nan_training_data, mean_nan_training_data, zero_nan_training_data, mean_nan_training_data)
+test_datasets = list(zero_nan_test_data, mean_nan_test_data, zero_nan_test_data, mean_nan_test_data)
+directories_names = list("boruta_zero_nan", "boruta_mean_nan", "boruta_removed_duplicates_zero_nan", "boruta_removed_duplicates_mean_nan")
 
-#zamiana wybranych zmiennych kategorycznych na dummy variables
-selected_cat_variables_columns = grep("Cat_", union_selected_attributes)
-selected_cat_variables_names = union_selected_attributes[selected_cat_variables_columns]
-
-selected_training_dummy_variables= c()
-selected_test_dummy_variables= c()
-
-for (cat_variable in selected_cat_variables_names){
-  selected_training_dummy_variables = cbind(selected_training_dummy_variables, dummy(cat_variable, traindata_zero, sep="_"))
-  selected_test_dummy_variables = cbind(selected_test_dummy_variables, dummy(cat_variable, test_data, sep="_"))
-}
-
-unique_to_training_data = setdiff(colnames(selected_training_dummy_variables), colnames(selected_test_dummy_variables))
-unique_to_test_data = setdiff(colnames(selected_test_dummy_variables), colnames(selected_training_dummy_variables))
-
-selected_test_dummy_variables = data.frame(selected_test_dummy_variables)
-selected_training_dummy_variables = data.frame(selected_training_dummy_variables)
-
-#dodanie brakujacych dummy variables do danych testowych
-selected_test_dummy_variables[, unique_to_training_data] = 0
-
-#usuniecie dummy variables nieobecnych w danych treningowych
-selected_test_dummy_variables = selected_test_dummy_variables[, !(names(selected_test_dummy_variables) %in% unique_to_test_data)]
-
-#zapisanie danych treningowych i testowych dla kazdej zmiennej Outcome w oddzielnym pliku
-for (k in 1:12){
-  column_selected_training_data = cbind(traindata_zero[(k+1)],traindata_zero[column_selected_attributes[[k]]])
-  column_selected_test_data = cbind(test_data[(k+1)],test_data[column_selected_attributes[[k]]])
-  write.csv(column_selected_training_data, file = sprintf("../Data_raw/BorutaSelectedTrainingData_Outcome_M%s.csv", k),row.names=FALSE)
-  write.csv(column_selected_test_data, file = sprintf("../Data_raw/BorutaSelectedTestData_Outcome_M%s.csv", k), row.names=FALSE)
+for(i in 1:4) {
+  boruta_result = boruta_results[[i]]
+  training_dataset = training_datasets[[i]]
+  test_dataset = test_datasets[[i]]
+  directory_name = directories_names[[i]]
   
-  column_selected_cat_variables_columns = grep("Cat_", column_selected_attributes[[k]])
-  column_selected_cat_variables_names = column_selected_attributes[[k]][column_selected_cat_variables_columns]
-  column_selected_dummy_variables_names = paste(column_selected_cat_variables_names, "_", sep="")
-  
-  column_selected_training_dummy_variables_columns = grep(paste(column_selected_dummy_variables_names, collapse="|"), colnames(selected_training_dummy_variables))
-  column_selected_training_dummy_variables = selected_training_dummy_variables[,column_selected_training_dummy_variables_columns]
-  
-  column_selected_test_dummy_variables_columns = grep(paste(column_selected_dummy_variables_names, collapse="|"), colnames(selected_test_dummy_variables))
-  column_selected_test_dummy_variables = selected_test_dummy_variables[,column_selected_test_dummy_variables_columns]
-  
-  column_selected_dummy_training_data = cbind(column_selected_training_data[ , !(names(column_selected_training_data) %in% column_selected_cat_variables_names)],column_selected_training_dummy_variables)
-  column_selected_dummy_test_data = cbind(column_selected_test_data[ , !(names(column_selected_test_data) %in% column_selected_cat_variables_names)],column_selected_test_dummy_variables)
-
-  write.csv(column_selected_dummy_training_data, file = sprintf("../Data_raw/BorutaSelectedDummyTrainingData_Outcome_M%s.csv", k),row.names=FALSE)
-  write.csv(column_selected_dummy_test_data, file = sprintf("../Data_raw/BorutaSelectedDummyTestData_Outcome_M%s.csv", k), row.names=FALSE)
-}
-
-#selected_training_data = cbind(traindata_zero[2:13],traindata_zero[union_selected_attributes])
-#write.csv(selected_training_data, file = "../Data_raw/BorutaSelectedTrainingData.csv", row.names=FALSE)
-
-#selected_test_data = cbind(test_data[2:13],test_data[union_selected_attributes])
-#write.csv(selected_test_data, file = "../Data_raw/BorutaSelectedTestData.csv", row.names=FALSE)
-
-#write.csv(selected_dummy_training_data, file = "../Data_raw/BorutaSelectedDummyTrainingData.csv", row.names=FALSE)
-
-#write.csv(selected_dummy_test_data, file = "../Data_raw/BorutaSelectedDummyTestData.csv", row.names=FALSE)
-
-#ponizej obliczenia dla celow sprawozdania
-
-#wyliczenie, ile atrybutow wybranych zostalo poszczegolne liczby razy (ile roznych atrybutow zostalo wskazanych raz, ile dwa razy itd.)
-results = matrix(, nrow = 12, ncol = 10)
-
-for (k in 1:12){
-  all_selected_attributes = boruta_results[[(k-1)*10 + 1]]
-  for (i in 2:10) {
-    all_selected_attributes = union(boruta_results[[(k-1)*10 + i]],all_selected_attributes)
-  }
-  number_of_occurences = all_selected_attributes
-  for (i in 1:length(all_selected_attributes)){
-    attribute = all_selected_attributes[i]
-    number = 0
-    for (j in 1:10){
-      if (attribute %in% boruta_results[[(k-1)*10 + j]]){
-        number = number + 1
-      }
+  #wybranie atrybutow wspolnych dla wszystkich prob dla danej zmiennej outcome
+  column_selected_attributes = matrix(rep(list(), 12),nrow = 10, ncol = 1)
+  for (k in 1:12){
+    intersect_selected_attributes = boruta_result[[(k-1)*10 + 1]]
+    for (i in 2:10) {
+      intersect_selected_attributes = intersect(boruta_result[[(k-1)*10 + i]],intersect_selected_attributes)
     }
-    number_of_occurences[i] = number
+    column_selected_attributes[[k]] = intersect_selected_attributes
   }
-  for (i in 1:10){
-    results[k,i] = length(which(number_of_occurences == i))
+  
+  #polaczenie wszystkich atrybutow dla wszystkich kolumn
+  union_selected_attributes = column_selected_attributes[[1]]
+  for (k in 2:12){
+    union_selected_attributes = union(column_selected_attributes[[k]],union_selected_attributes)
+  }
+  
+  #zamiana wybranych zmiennych kategorycznych na dummy variables
+  selected_cat_variables_columns = grep("Cat_", union_selected_attributes)
+  selected_cat_variables_names = union_selected_attributes[selected_cat_variables_columns]
+  
+  selected_training_dummy_variables= c()
+  selected_test_dummy_variables= c()
+  
+  for (cat_variable in selected_cat_variables_names){
+    selected_training_dummy_variables = cbind(selected_training_dummy_variables, dummy(cat_variable, training_dataset, sep="_"))
+    selected_test_dummy_variables = cbind(selected_test_dummy_variables, dummy(cat_variable, test_dataset, sep="_"))
+  }
+  
+  unique_to_training_data = setdiff(colnames(selected_training_dummy_variables), colnames(selected_test_dummy_variables))
+  unique_to_test_data = setdiff(colnames(selected_test_dummy_variables), colnames(selected_training_dummy_variables))
+  
+  selected_test_dummy_variables = data.frame(selected_test_dummy_variables)
+  selected_training_dummy_variables = data.frame(selected_training_dummy_variables)
+  
+  #dodanie brakujacych dummy variables do danych testowych
+  selected_test_dummy_variables[, unique_to_training_data] = 0
+  
+  #usuniecie dummy variables nieobecnych w danych treningowych
+  selected_test_dummy_variables = selected_test_dummy_variables[, !(names(selected_test_dummy_variables) %in% unique_to_test_data)]
+  
+  #zapisanie danych treningowych i testowych dla kazdej zmiennej Outcome w oddzielnym pliku
+  for (k in 1:12){
+    column_selected_training_data = cbind(training_dataset[(k+1)],training_dataset[column_selected_attributes[[k]]])
+    column_selected_test_data = cbind(test_dataset[(k+1)],test_dataset[column_selected_attributes[[k]]])
+    write.csv(column_selected_training_data, file = sprintf("../Data_raw/%s/BorutaSelectedTrainingData_Outcome_M%s.csv", directory_name, k),row.names=FALSE)
+    write.csv(column_selected_test_data, file = sprintf("../Data_raw/%s/BorutaSelectedTestData_Outcome_M%s.csv", directory_name, k), row.names=FALSE)
+    
+    column_selected_cat_variables_columns = grep("Cat_", column_selected_attributes[[k]])
+    column_selected_cat_variables_names = column_selected_attributes[[k]][column_selected_cat_variables_columns]
+    column_selected_dummy_variables_names = paste(column_selected_cat_variables_names, "_", sep="")
+    
+    column_selected_training_dummy_variables_columns = grep(paste(column_selected_dummy_variables_names, collapse="|"), colnames(selected_training_dummy_variables))
+    column_selected_training_dummy_variables = selected_training_dummy_variables[,column_selected_training_dummy_variables_columns]
+    
+    column_selected_test_dummy_variables_columns = grep(paste(column_selected_dummy_variables_names, collapse="|"), colnames(selected_test_dummy_variables))
+    column_selected_test_dummy_variables = selected_test_dummy_variables[,column_selected_test_dummy_variables_columns]
+    
+    column_selected_dummy_training_data = cbind(column_selected_training_data[ , !(names(column_selected_training_data) %in% column_selected_cat_variables_names)],column_selected_training_dummy_variables)
+    column_selected_dummy_test_data = cbind(column_selected_test_data[ , !(names(column_selected_test_data) %in% column_selected_cat_variables_names)],column_selected_test_dummy_variables)
+    
+    write.csv(column_selected_dummy_training_data, file = sprintf("../Data_raw/%s/BorutaSelectedDummyTrainingData_Outcome_M%s.csv", directory_name, k),row.names=FALSE)
+    write.csv(column_selected_dummy_test_data, file = sprintf("../Data_raw/%s/BorutaSelectedDummyTestData_Outcome_M%s.csv", directory_name, k), row.names=FALSE)
   }
 }
-
-#wyliczenie liczby wybran (przez kazda zmienna outcome z osobna) dla poszczegolnych wybranych atrybutow
-numbers = c(rep(0,12))
-for (attribute in union_selected_attributes){
-  number = 0
-  for (j in 1:12){
-    if (attribute %in% column_selected_attributes[[j]]){
-      number = number + 1
-    }
-  }
-  numbers[number] = numbers[number] + 1
-}
-
-
-
-
-
-
-
-
