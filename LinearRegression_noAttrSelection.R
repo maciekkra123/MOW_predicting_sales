301789a8b55a692e5a676843c11f30c0b4fd43b5
maciekkra <maciek_krasowski@yahoo.pl>
2018-06-10 12:44:14 +0200

    Dokumentacja kodu

--- LinearRegression_noAttrSelection.R
+++
@@ -1,6 +0,0 @@
-library(caret)
-Data_all <- read.csv("Data_raw/Dataset_old.csv", na.strings="NaN")
-View(Data_all) #wglad w dane (widok tabelaryczny)
-
-test_kaggle <- read.csv("Kaggle_data/TestDataset.csv")
-View(test_kaggle)
